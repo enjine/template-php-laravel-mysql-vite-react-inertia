63f5c0fe117a108de892c2a82439754ac85d4407
Alessandro Di Candia <6955491+enjine@users.noreply.github.com>
2024-09-05 16:51:53 +0000

    chore: add TODO for SSR

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {createInertiaApp } from '@inertiajs/inertia-react'
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+// TODO: figure SSR out
 //import { hydrateRoot } from 'react-dom/client'
 import "./bootstrap";
 import 'css/app.css'
